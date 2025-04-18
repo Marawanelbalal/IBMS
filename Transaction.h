43d1cd0229dff11a405c74b06f977c553fcbe38c
Marawan-Abdulrahim <s-marawan.elbalal@zewailcity.edu.eg>
2025-04-18 15:35:08 +0200

    Deleted test files

--- Transaction.h
+++ Transaction.h
@@ -1,15 +1,39 @@
-#pragma once
+#include <string>
+#include <vector>
+#include <ctime>
+// #include "Account.h" // Include the Account header
 
-//Transaction.h can't contain Account.h because Account.h contains Transaction.h
-//Forward declare the class to avoid circular dependency
-//PLACEHOLDER transaction class
-class Transaction
-{
-	
+class Transaction {
+private:
+    std::string transactionID;
+    double amount;
+    std::time_t timestamp;
+    std::string transactionType; // deposit, withdrawal, transfer, etc.
+    std::vector<std::string> involvedAccountNumbers; // Account numbers involved
+    bool isValid;
+    std::string description;
+
 public:
-	int acc1ID;
-	int acc2ID;
-	float amount;
-	Transaction(int acc1ID, int acc2ID, float amount);
+    // Constructor
+    Transaction(const std::string& id, double amt, const std::string& type, 
+                const std::vector<std::string>& accounts);
+    
+    // Execute the transaction
+    virtual bool execute();
+    
+    // Validate if the transaction can be performed
+    virtual bool validateTransaction();
+    
+    // Getters
+    double getAmount() const;
+    std::time_t getTimestamp() const;
+    std::string getTransactionType() const;
+    std::string getTransactionID() const;
+    std::vector<std::string> getInvolvedAccounts() const;
+    bool getIsValid() const;
+    std::string getDescription() const;
+    
+    // Setters
+    void setDescription(const std::string& desc);
+    void setIsValid(bool valid);
 };
-
--- findAccount.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Customer.h"
-
-class findAccount
-{
-public:
-	Account& getAccountWithID(Customer& C, Account& A);
-};
-
