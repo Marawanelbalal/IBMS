2f4aefb9e42de3583a6ff01c272cdef6434fce50
xX-youssuf-Xx <youssufhosam@gmail.com>
2025-04-19 21:00:41 +0200

    implemented the bank class with core functionalities

--- main.cpp
+++ main.cpp
@@ -4,47 +4,60 @@
 #include "Customer.h"
 #include "Account.h"
 #include "Transaction.h"
-#include "user.h"
+#include "User.h"
+#include "Bank.h"
+#include "UI.h"
 
 using namespace std;
 
 int main() {
-    // Create accounts with the correct constructor parameters
-    Account acc1("USD", "John Doe1", 500.0, 12345);
-    Account acc2("USD", "John Doe1", 1000.0, 67890);
-    Account acc3("USD", "John Doe2", 150.0, 11111);
+    // Create a bank instance
+    Bank bank;
 
-    // Create a customer
-    Customer customer1("John Doe1","1019", "1234567890");
-    Customer customer2("John Doe1","1020", "1234567890");
+    // Initialize demo data
+    bank.initializeDemoData();
 
+    // Display all users and accounts
+    bank.viewAllUsers();
+    bank.viewAllAccounts();
 
-    // Add accounts to the customer
-    customer1.addAccount(acc1);
-    customer1.addAccount(acc2);
-    customer2.addAccount(acc3);
+    // Get customers by name
+    Customer* customer1 = bank.getCustomerByName("John Doe1");
+    Customer* customer2 = bank.getCustomerByName("John Doe2");
 
-    // Display initial account information
-    cout << "Initial account balances:" << endl;
-    customer1.viewOwnAccounts();
-    customer2.viewOwnAccounts();
+    if (customer1 && customer2) {
+        // Display initial account information
+        cout << "Initial account balances:" << endl;
+        customer1->viewOwnAccounts();
+        customer2->viewOwnAccounts();
 
-    // Perform some transfers
-    cout << "\nTransferring 200 from acc1 to acc2..." << endl;
-    customer1.transferMoney(acc1, acc2,customer1, 200.0);
+        // Get account IDs
+        vector<Account> accounts1 = customer1->getAccounts();
+        vector<Account> accounts2 = customer2->getAccounts();
 
-    cout << "\nTransferring 50 from acc2 to acc3..." << endl;
-    customer1.transferMoney(acc2, acc3,customer2, 50.0);
+        if (!accounts1.empty() && !accounts2.empty()) {
+            int acc1Id = accounts1[0].getAccountNumber();
+            int acc2Id = accounts1[1].getAccountNumber();
+            int acc3Id = accounts2[0].getAccountNumber();
 
-    cout << "\nTransferring 100 from acc3 to acc1..." << endl;
-    customer2.transferMoney(acc3, acc1, customer1, 100.0);
-    
-    customer1.transferMoney(acc1, acc3, customer2, 50.0);
-    // Display final account information
-    cout << "\nFinal account balances:" << endl;
-    customer1.viewOwnAccounts();
-    customer2.viewOwnAccounts();
+            // Perform transfers
+            cout << "\nTransferring 200 from acc1 to acc2..." << endl;
+            bank.transferMoney("John Doe1", acc1Id, "John Doe1", acc2Id, 200.0);
 
+            cout << "\nTransferring 50 from acc2 to acc3..." << endl;
+            bank.transferMoney("John Doe1", acc2Id, "John Doe2", acc3Id, 50.0);
+
+            cout << "\nTransferring 100 from acc3 to acc1..." << endl;
+            bank.transferMoney("John Doe2", acc3Id, "John Doe1", acc1Id, 100.0);
+
+            bank.transferMoney("John Doe1", acc1Id, "John Doe2", acc3Id, 50.0);
+
+            // Display final account information
+            cout << "\nFinal account balances:" << endl;
+            customer1->viewOwnAccounts();
+            customer2->viewOwnAccounts();
+        }
+    }
 
     return 0;
 }
